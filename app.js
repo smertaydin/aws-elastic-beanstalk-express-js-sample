ec61a22615d2173fbf24e04bed22c5130e9fbf1a
smertaydin <smertaydin@me.com>
2021-06-03 19:57:05 +0300

    change

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
-const express = require('express');
-const app = express();
-const port = 8080;
+// noinspection JSUnusedLocalSymbols
+exports.handler = async (event, context) => {
+	console.log("Handling event:", JSON.stringify(event));
 
-app.get('/', (req, res) => res.send('Mert!'));
+	const body = event.body ? JSON.parse(event.body) : {};
+	// noinspection JSUnresolvedVariable
+	const input1 = body.input1 || 0;
+	// noinspection JSUnresolvedVariable
+	const input2 = body.input2 || 0;
+	const output = input1 + input2;
 
-app.listen(port);
-console.log(`App running on http://localhost:${port}`);
+	const response = {
+		statusCode: 200,
+		body: JSON.stringify({
+			output: output
+		}),
+	};
+	console.log("Result:", JSON.stringify(response));
+
+	return response;
+};
