804ad8fbd4097652b890e54f86cd6caa0a1ad956
smertaydin <81705973+smertaydin@users.noreply.github.com>
2021-06-14 11:07:58 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
-// noinspection JSUnusedLocalSymbols
 exports.handler = async (event, context) => {
 	console.log("Handling event:", JSON.stringify(event));
-
 	const body = event.body ? JSON.parse(event.body) : {};
-	// noinspection JSUnresolvedVariable
 	const input1 = body.input1 || 0;
-	// noinspection JSUnresolvedVariable
 	const input2 = body.input2 || 0;
 	const output = input1 + input2;
-
 	const response = {
 		statusCode: 200,
 		body: JSON.stringify({
@@ -16,6 +11,5 @@ exports.handler = async (event, context) => {
 		}),
 	};
 	console.log("Result:", JSON.stringify(response));
-
 	return response;
 };
